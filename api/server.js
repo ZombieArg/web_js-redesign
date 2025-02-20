6cbd5c303217b17baaa49b07433f45423e602394
Tomas Gomez Cuevas <tomasgomezc88@gmail.com>
2025-02-20 17:09:22 -0300

    change variable name

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(cors({
-    origin: process.env.API_URL
+    origin: process.env.CLIENT_URL
 }));
 
 const mailgun = new Mailgun(formData);
